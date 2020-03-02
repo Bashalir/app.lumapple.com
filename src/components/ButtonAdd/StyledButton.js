f4b55e86c0922b4651ceac3b80009614776a5478
Bashalir <bachir@sahali.fr>
2020-03-02 08:19:12 +0100

    Add Animation

--- src/components/ButtonAdd/StyledButton.js
+++ src/components/ButtonAdd/StyledButton.js
@@ -18,17 +18,35 @@ box-shadow:  4px 4px 7px ${({ theme }) => theme.colors.shadowDarker},
              -4px -4px 7px ${({ theme }) => theme.colors.shadowLight};
   color:${({ theme }) => theme.colors.neutralLighter};
 padding:5px 0;
-
-
-:hover {
+position:relative;
+outline: 0;
+::after{
+  content:"";
+  position:absolute;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+  opacity:0;
+  border-radius: 19px;
   box-shadow: inset 4px 4px 11px ${({ theme }) => theme.colors.primaryDark},
             inset -4px -4px 11px ${({ theme }) => theme.colors.shadowLight};
+  transition: opacity 250ms;
+}
+:hover {
+            ::after {
+              opacity:1;
+            }
 }
 
 :active{
-  color:${({ theme }) => theme.colors.primaryDark};
+  .span-add{
+
+  ::after {opacity:1;}
+  }
   .logo-add {
-    background-image: url('${AddActive}');
+
+    ::after {opacity:1;}
   }
 }
 
@@ -39,16 +57,50 @@ padding:5px 0;
     background-size:contain;
     background-position:center;
 
+    position:relative;
+
     width:25px;
     height:25px;
     padding: 0 5px 0 15px;
     z-index:1;
+    ::after{
+      z-index:-1;
+      content:"";
+      position:absolute;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+  opacity:0;
+  background-image: url('${AddActive}');
+  background-repeat: no-repeat;
+    background-size:contain;
+    background-position:center;
+    transition: opacity 250ms;
+    }
   }
   .span-add{
 
     padding: 0 15px 0 0px;
     font-size:1.2em;
     text-align:center;
+    position:relative;
+z-index:1;
+
+    ::after{
+
+      padding: 0 15px 0 0px;
+    text-align:center;
+      content:"Vendez maintenant";
+      position:absolute;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+  opacity:0;
+      color:${({ theme }) => theme.colors.primaryDark};
+      transition: opacity 250ms;
+    }
   }
 
 `;
